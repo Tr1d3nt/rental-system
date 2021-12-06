086c63333c7f171810731f919797eb2fb114e3d9
Tr1d3nt <saudagha2@gmail.com>
2021-12-06 00:27:50 -0700

    started notification stuff

---
+++ src/controllers/NotificationsController.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import tasks.*;
+import java.sql.*;
+import java.util.*;
+
+public class NotificationsController extends DBController {
+
+    public NotificationsController() {
+        initializeConnection();
+    }
+
+    public void addNotification(Notifications notification) {
+
+        try {
+
+            String query = "INSERT INTO notifications (renterID, type, bedrooms, bathrooms, furnished, quadrant)"
+                    + " VALUES (?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement stmt = dbConnect.prepareStatement(query);
+
+            stmt.setInt(0, notification.getID());
+            stmt.setString(1, notification.getType());
+            stmt.setInt(2, notification.getBedroom());
+            stmt.setInt(3, notification.getBathroom());
+            stmt.setString(4, notification.getFurnished());
+            stmt.setString(5, notification.getQuadrant());
+
+            stmt.execute();
+            stmt.close();
+
+            props.add(property);
+            notifyObservers();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void initializeConnection() {
+
+        try {
+
+            this.dbConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+            System.out.println("Connection established!\n");
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    public void close() {
+
+        try {
+
+            dbConnect.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
